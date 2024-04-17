9c13f1816975ec955d15f1bfa8d9cd10de323d1d
Yuliia Frund <yuliia.frund@uzh.ch>
2024-04-17 14:14:36 +0200

    edited download_data.sh to download a different dataset (Art of Perfumery)

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -20,24 +20,24 @@ done
 
 # download a different interesting data set!
 
-mkdir -p $data/grimm
+mkdir -p $data/art_of_perfumery
 
-mkdir -p $data/grimm/raw
+mkdir -p $data/art_of_perfumery/raw
 
-wget https://www.gutenberg.org/files/52521/52521-0.txt
-mv 52521-0.txt $data/grimm/raw/tales.txt
+wget https://www.gutenberg.org/cache/epub/16378/pg16378.txt
+mv pg16378.txt $data/art_of_perfumery/raw/perfume.txt
 
 # preprocess slightly
 
-cat $data/grimm/raw/tales.txt | python $base/scripts/preprocess_raw.py > $data/grimm/raw/tales.cleaned.txt
+cat $data/art_of_perfumery/raw/perfume.txt | python $base/scripts/preprocess_raw.py > $data/art_of_perfumery/raw/perfume.cleaned.txt
 
 # tokenize, fix vocabulary upper bound
 
-cat $data/grimm/raw/tales.cleaned.txt | python $base/scripts/preprocess.py --vocab-size 5000 --tokenize --lang "en" --sent-tokenize > \
-    $data/grimm/raw/tales.preprocessed.txt
+cat $data/art_of_perfumery/raw/perfume.cleaned.txt | python $base/scripts/preprocess.py --vocab-size 5000 --tokenize --lang "en" --sent-tokenize > \
+    $data/art_of_perfumery/raw/perfume.preprocessed.txt
 
 # split into train, valid and test
 
-head -n 440 $data/grimm/raw/tales.preprocessed.txt | tail -n 400 > $data/grimm/valid.txt
-head -n 840 $data/grimm/raw/tales.preprocessed.txt | tail -n 400 > $data/grimm/test.txt
-tail -n 3075 $data/grimm/raw/tales.preprocessed.txt | head -n 2955 > $data/grimm/train.txt
+head -n 440 $data/art_of_perfumery/raw/perfume.preprocessed.txt | tail -n 400 > $data/art_of_perfumery/valid.txt
+head -n 840 $data/art_of_perfumery/raw/perfume.preprocessed.txt | tail -n 400 > $data/art_of_perfumery/test.txt
+tail -n 3075 $data/art_of_perfumery/raw/perfume.preprocessed.txt | head -n 2955 > $data/art_of_perfumery/train.txt
